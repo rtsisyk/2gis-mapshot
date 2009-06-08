c83c56c26481cd9daba2322ac34a435393e3cde2
Roman O Tsisyck <inbox@art1x.com>
2009-06-08 20:39:12 +0700

    added lost resource.h

---
+++ MapShotPlugin/resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MapShotPlugin.rc
+//
+#define IDS_CMDSAVEIMAGE                101
+#define IDD_BITMAPDLG                   101
+#define IDD_MAPSHOT                     101
+#define IDS_CMDMAKESHOT                 101
+#define IDS_CMDMAKESHOT_DESC            102
+#define IDC_RESIZEH                     104
+#define IDS_MAPSIZE                     104
+#define IDC_RESIZEV                     107
+#define IDC_SELECTIONMOVE               109
+#define IDC_RESIZECORNER                110
+#define IDC_MAPHAND                     113
+#define IDB_CMDMAKESHOT                 118
+#define IDC_WIDTHEDIT                   204
+#define IDC_HEIGHTEDIT                  205
+#define IDC_SCALESPIN                   211
+#define IDC_SCALEEDIT                   212
+#define IDC_SAVEBUTTON                  214
+#define IDC_CANCELBUTTON                215
+#define IDC_SCALESTATIC                 216
+#define IDC_SIZESTATIC                  217
+#define IDC_SIZESTATIC2                 218
+#define IDC_LEFTICON                    1001
+#define IDC_STATICAREA                  1003
+#define IDC_MAPSIZE                     1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        119
+#define _APS_NEXT_COMMAND_VALUE         40002
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
