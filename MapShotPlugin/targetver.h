1c483dfd48982eadf2c7f2cd571669eb77bbcf0b
Roman Tsisyk <roman@tsisyk.com>
2012-05-13 18:25:55 +0700

    Update copyrights

--- MapShotPlugin/targetver.h
+++ MapShotPlugin/targetver.h
@@ -1,6 +1,6 @@
 /*******************************************************************************
 *  This file is part of the MapShot plugin
-*  Copyright (C) 2009 Roman O Tsisyck <inbox@art1x.com>
+*  Copyright (C) 2009 Roman Tsisyk <roman@tsisyk.com>
 *
 *  This library is free software; you can redistribute it and/or
 *  modify it under the terms of the GNU Lesser General Public
